ca923adb6cab095c24374bbd33530bead4d696d7
Deryx E. Scott <deryxescott@gmail.com>
2016-01-06 00:07:17 -0500

    Initial commit

---
+++ scripts/application.js
@@ -0,0 +1,27 @@
+$(function() {
+	var camperNews = "http://www.freecodecamp.com/news/hot";
+	$.getJSON(camperNews, function(json) {
+		$.each(json, function() {
+			var avatar = "<img src='" + this.author.picture + "' />";
+			
+			var username = "<p class='user'><a href='http://www.freecodecamp.com/" + this.author.username + "' target='_blank'>" + this.author.username + "</a></p>"
+			
+			var headlineLink = this.link;
+			var headline = "<p><a href='" + headlineLink + "' target='_blank'>" + this.headline + "</a></p>";
+			
+			var numberUpvotes = this.upVotes.length
+			var upVotes = "<p class='votes'>upvotes&nbsp;&nbsp;" + numberUpvotes + "</p>";
+			
+			var card = "<div class='news'>";
+			
+			card += avatar;
+			card += username;
+			card += headline;
+			card += upVotes;
+			
+			card += "</div>";
+			
+			$('#container').append(card);
+		});
+	});
+});
